7222ecd67dafecc93978bee3bc550977caf740cd
vishal <mgvishalctr@gmail.com>
2025-10-08 23:25:39 +0530

    commit

--- javascript/destructing.js
+++ javascript/destructing.js
@@ -1,8 +1,17 @@
+// ---------- Example 1: Object Destructuring ----------
 let student = { sname: "John", age: 20, dept: "CSE", marks: 420 };
-console.log(student);
+console.log("Student Object:", student);
 
-// Destructuring age and marks, and collecting the rest into 'details'
+// Destructuring object
 let { age, marks, ...details } = student;
+console.log("Age:", age, "Marks:", marks);
+console.log("Other Details:", details);
 
-console.log(age, marks);
-console.log(details);
+// ---------- Example 2: Array Destructuring ----------
+let fruits = ['apple', 'mango', 'litchi', 'grapes'];
+console.log("\nFruits Array:", fruits);
+
+// Destructuring array
+let [a, m, ...res] = fruits;
+console.log("First Fruit:", a, "Second Fruit:", m);
+console.log("Remaining Fruits:", res);
