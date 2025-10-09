3f3cfb9800f537a3cf867977845de0a3d86892fc
vishal <mgvishalctr@gmail.com>
2025-10-10 00:10:00 +0530

    commit

--- javascript/bom and dom/app.js
+++ javascript/bom and dom/app.js
@@ -13,3 +13,24 @@ function changeText() {
     document.body.appendChild(newElement);
   }
   
+  
+// Display current URL
+console.log("Current URL:", window.location.href);
+
+// Alert, confirm, prompt
+alert("Welcome to BOM Example!");
+let name = prompt("Enter your name:");
+if (confirm("Do you want to continue?")) {
+  console.log("Hello " + name + "!");
+}
+
+// Show screen details
+console.log("Screen width:", screen.width);
+console.log("Screen height:", screen.height);
+
+// Redirect example
+// window.location.href = "https://www.google.com";
+
+// Open and close window
+// let newWin = window.open("https://www.example.com");
+// newWin.close();
